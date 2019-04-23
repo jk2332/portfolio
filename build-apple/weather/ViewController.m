7ff994ce188e1d184497acdc73c5c5762757e25c
Stefan Joseph <Stefan@dhcp-hol-646.redrover.cornell.edu>
2019-04-23 10:39:43 -0400

    got TestFlight working

---
+++ build-apple/weather/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  weather
+//
+//  Created by Stefan Joseph on 4/21/19.
+//  Copyright © 2019 Cornell Game Design Initiative. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
